87bbf185188038a44c008f95872512da184a6f1c
RSmith-TD <rogan.smith@hotmail.co.uk>
2022-02-14 14:33:34 +0000

    finished JS validation
    Co-authored-by: Shadeojo1 <Shadeojo1@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,70 +3,93 @@ const fnameInput = document.querySelector('#fname');//.addEventListener('change'
 const lnameInput = document.querySelector('#lname');//.addEventListener('change', isAlphabetic);
 //document.querySelector('#lname');//.addEventListener('change', isCorrectLength);
 const emailInput = document.querySelector('#email');//.addEventListener('change', emailCorrectFormat);
+const signupButton = document.querySelector(`#signup`)
 
 let fNameError = document.querySelector('#fnameerror');
 let lNameError = document.querySelector('#lnameerror');
 let emailError = document.querySelector('#emailerror');
-let phoneError = document.querySelector('#phoneerror');
+let signupError = document.querySelector('#signuperror');
 
 let fnameValid = false;
 let lnameValid = false;
 let emailValid = false;
 
 fnameInput.addEventListener(`change`, e => {
+    fnameValid = true;
+    fNameError.textContent = ``;
     if (isAlphabetic(e.target.value)) {
         //Make it not invalid
     } else {
         fNameError.textContent = `Only lower and upper case letters are allowed`;
-        //Make it invalid
+        fnameValid = false;
     }
 
     if (isCorrectLength(e.target.value)) {
         //Make it not invalid
     } else {
-        console.log(`fnameInput.textContent: ${fnameInput.textContent}`)
+        // console.log(`fnameInput.textContent: ${fnameInput.textContent}`)
         fNameError.textContent = `The First Name entered is either too short or too long`;
-        //Make it invalid
+        fnameValid = false;
     }
 
-    fnameValid = isAlphabetic(fnameInput) && isCorrectLength(fnameInput);
+    checkIfAllIsValid();
+
 }, false);
 
 lnameInput.addEventListener(`change`, e => {
+    lnameValid = true;
+    lNameError.textContent = ``;
     if (isAlphabetic(e.target.value)) {
         //Make it not invalid
     } else {
         lNameError.textContent = `Only lower and upper case letters are allowed`;
-        //Make it invalid
+        lnameValid = false;
     }
 
     if (isCorrectLength(e.target.value)) {
         //Make it not invalid
     } else {
         lNameError.textContent = `The Last Name entered is either too short or too long`;
-        //Make it invalid
+        lnameValid = false;
     }
 
-    lnameValid = isAlphabetic(lnameInput) && isCorrectLength(lnameInput);
+    checkIfAllIsValid();
+
 }, false);
 
 emailInput.addEventListener(`change`, e => {
+    emailValid = true;
+    emailError.textContent = ``
     if (emailCorrectFormat(e.target.value)) {
         //Make not invalid
     } else {
         emailError.textContent = `Email address not valid`;
+        emailValid = false;
     }
 
-    emailValid = emailCorrectFormat(emailInput);
+    checkIfAllIsValid();
 }, false);
+
+
+// signupButton.addEventListener(`click`, e => {
+//     signupError.textContent = ``;
+//     if (checkIfAllIsValid()) {
+
+//     } else {
+
+//         signupError.textContent = `One or more required elements are not valid, please try again`;
+//     }
+
+// }, false);
 
 
 function isAlphabetic(stringToParse) {
 
-    if (stringToParse./*toLowerCase().*/includes == 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+    // if (stringToParse./*toLowerCase().*/includes == 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+    if (/^[a-zA-Z]+$/.test(stringToParse)) {
         return true;
     } else {
-        console.log('Only lower and upper case letters are allowed')
+        // console.log('Only lower and upper case letters are allowed')
         return false;
     };
 
@@ -78,7 +101,7 @@ function isCorrectLength(stringToParse) {
     if (stringToParse.length > 1 && stringToParse.length < 15) {
         return true;
     } else {
-        console.log('The First Name entered is either too short or too long.');
+        // console.log('The First Name entered is either too short or too long.');
         return false;
     };
 };
@@ -95,23 +118,33 @@ function isCorrectLength(stringToParse) {
 
 
 
+// function emailCorrectFormat(stringToParse) {
+//     if (stringToParse.length > 1 && stringToParse.includes('@') && stringToParse.includes('.')) {
+//         return true;
+//     } else {
+//         console.log('Email address not valid')
+//         return false;
+//     }
+// };
+
 function emailCorrectFormat(stringToParse) {
-    if (stringToParse.length > 1 && stringToParse.includes('@')) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(stringToParse)) {
         return true;
-    } else {
-        console.log('Email address not valid')
-        return false;
     }
-};
+    // console.log("You have entered an invalid email address!")
+    return false;
+}
 
 
 
-
-
-function submitButton() {
-    if (fNameError === true) {
+function checkIfAllIsValid() {
+    // console.log(`fnameValid: ${fnameValid}, lnameValid: ${lnameValid}, emailValid: ${emailValid}`);
+    if (fnameValid && lnameValid && emailValid) {
+        signupButton.disabled = false;
         return true
     } else {
+        signupError.textContent = `One or more required elements are not valid`;
+        signupButton.disabled = true;
         return false
     }
 
